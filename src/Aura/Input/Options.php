61079e7f18162f1f5dbde006d336e52251dec1e0
Paul M. Jones <pmjones88@gmail.com>
2013-02-15 12:50:42 -0600

    actually add the options class. thanks @harikt

---
+++ src/Aura/Input/Options.php
@@ -0,0 +1,9 @@
+<?php
+namespace Aura\Input;
+
+class Options
+{
+    // entirely up to the user to fill with methods for getting input options.
+    // useful for providing information about the environemnt, or for common
+    // values like month, day, year, etc.
+}
